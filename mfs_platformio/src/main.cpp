e6ad5b8627fec90cd2b166f7116a295a6c810c90
Lluís Francesc Collell Erra <uiscollell@gmail.com>
2026-04-18 18:12:31 +0200

    Update main.cpp

--- mfs_platformio/src/main.cpp
+++ mfs_platformio/src/main.cpp
@@ -1,21 +1,22 @@
   #include <Arduino.h>
   #include <SPI.h>
 
-  #define LED1_PIN = 
+  #define LEDIR_PIN = 25;
+  #define LEDIR_PIN = 26;
   #define DRDY_PIN = 19;
-  hw_timer_t * timerLed1 = NULL;
-  hw_timer_t * timerLed2 = NULL;
+  hw_timer_t * timerLedIR = NULL;
+  hw_timer_t * timerLedR = NULL;
   volatile bool dataReadyFlag = false;
-  volatile bool led1 = false;
-  volatile bool led2 = false;
+  volatile bool ledir = false;
+  volatile bool led4 = false;
 
   void IRAM_ATTR isr_drdy() {
     dataReadyFlag = true;
   }
-  void IRAM_ATTR onLed1() {
+  void IRAM_ATTR onLedIR() {
     led1 = true;
   }
-  void IRAM_ATTR onLed2() {
+  void IRAM_ATTR onLedR() {
     led2 = true;
   }
 
@@ -23,24 +24,24 @@
     Serial.begin(115200);
 
     pinMode(DRDY_PIN, INPUT);
-    pinMode(LED1_PIN, OUTPUT);
-    pinMode(LED2_PIN, OUTPUT);
+    pinMode(LEDIR_PIN, OUTPUT);
+    pinMode(LEDR_PIN, OUTPUT);
 
     // Configurar interrupció per flanc de baixada
     attachInterrupt(digitalPinToInterrupt(DRDY_PIN), isr_drdy, FALLING); //he posat rising ja que vull interpretar quan comença el puls
     Serial.println("Sistema preparat...");
 
     // Timer 1
-    timerLed1 = timerBegin(0, xx, true);
-    timerAttachInterrupt(timer1, &onLed1, true);
-    timerAlarmWrite(timer1, yyyyy, true);
-    timerAlarmEnable(timer1);
+    timerLedIR = timerBegin(0, xx, true);
+    timerAttachInterrupt(timerIR, &onLedIR, true);
+    timerAlarmWrite(timerIR, yyyyy, true);
+    timerAlarmEnable(timerIR);
 
     // Timer 2
-    timerLed2 = timerBegin(1, xx, true);
-    timerAttachInterrupt(timer2, &onLed2, true);
-    timerAlarmWrite(timer2, yyyyy, true);
-    timerAlarmEnable(timer2); 
+    timerLedR = timerBegin(1, xx, true);
+    timerAttachInterrupt(timerR, &onLedR, true);
+    timerAlarmWrite(timerR, yyyyy, true);
+    timerAlarmEnable(timerR); 
   }
 
   void loop() {
@@ -52,13 +53,15 @@
       attachInterrupt(digitalPinToInterrupt(DRDY_PIN), isr_drdy, FALLING);
     }
 
-    if (led1) {
-      led1 = false;
+    if (ledIR) {
+      ledIR = false;
       //Configurar timer per recomençar la conta enere.
 
     }
     
-    if (led2) {
+    if (ledR) {
+      ledIR = false;
+      //Configurar timer per recomençar la conta enere.
     }
 
     delay(10); // petita pausa
