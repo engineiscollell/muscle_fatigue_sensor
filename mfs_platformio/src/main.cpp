05f38067ecf89d286635bf5f2bce4f158dd87a1f
Lluís Francesc Collell Erra <uiscollell@gmail.com>
2026-04-19 20:19:39 +0200

    petits ajustos al codi + notes sobre futurs pasos del codi

--- mfs_platformio/src/main.cpp
+++ mfs_platformio/src/main.cpp
@@ -19,6 +19,77 @@
   void IRAM_ATTR onLedR() {
     led2 = true;
   }
+  
+  //DEFINICIÓ TASQUES
+  /*
+  // Declarem el task handle
+  TaskHandle_t BlinkTaskHandle = NULL;
+  void BlinkTask(void *parameter) {
+    for (;;) { // llaç infinit!!!
+      digitalWrite(LED_PIN, HIGH);
+      Serial.println("BlinkTask: LED ON");
+      vTaskDelay(1000 / portTICK_PERIOD_MS); // 1000ms
+      digitalWrite(LED_PIN, LOW);
+      Serial.println("BlinkTask: LED OFF");
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+      Serial.print(“Encén LED executant-se al core ");
+      Serial.println(xPortGetCoreID());
+    }
+  }
+  
+  //Task1code: blinks an LED every 1000 ms
+  void Task1code( void * pvParameters ){
+    Serial.print("Task1 running on core ");
+    Serial.println(xPortGetCoreID());
+    for(;;){
+      digitalWrite(led1, HIGH);
+      delay(1000);
+      digitalWrite(led1, LOW);
+      delay(1000);
+      }
+  }
+
+  //Task2code: blinks an LED every 700 ms
+  void Task2code( void * pvParameters ){
+    Serial.print("Task2 running on core ");
+    Serial.println(xPortGetCoreID());
+    for(;;){
+      digitalWrite(led2, HIGH);
+      delay(700);
+      digitalWrite(led2, LOW);
+      delay(700);
+    }
+  }
+  */
+   
+//DEFINICIÓ TASQUES
+/*
+void adcInit();
+Configurar AD7172-2
+ODR
+Filtre
+Referència
+Canal
+Mode de lectura
+
+void bleInit();
+Servei BLE
+Caracteristica
+Advertising
+
+void processingInit();
+Inicialitzar filtres
+Variables de baseline
+Estat del processament
+
+void createRTOSObjects();
+crear cua
+crear mutex
+
+void createTasks();
+crear tasques
+Fixar tasques a un core concret
+*/
 
   void setup() {
     Serial.begin(115200);
@@ -42,6 +113,29 @@
     timerAttachInterrupt(timerR, &onLedR, true);
     timerAlarmWrite(timerR, yyyyy, true);
     timerAlarmEnable(timerR); 
+
+    //TASQUES 
+    /*
+    xTaskCreatePinnedToCore(
+      BlinkTask, // Nom funció de la tasca
+      “Encén LED", // Nom tasca (el que vulgueu)
+      10000, // Stack size (x4 bytes)
+      NULL, // Parametres
+      1, // Prioritat
+      &BlinkTaskHandle, // Task handle
+      1 // Core al que s’executa
+    );
+
+    */
+
+    //INICIALITZACIONS
+    systemInit();
+    hardwareInit();
+    adcInit();
+    bleInit();
+    processingInit();
+    createRTOSObjects();
+    createTasks();
   }
 
   void loop() {
